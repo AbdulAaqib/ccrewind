f91e23667c392dac24361c5874fe205c71c3ae08
AbdulAaqib <abdulaaqib4@gmail.com>
2026-04-06 00:29:14 +0100

    removed, cloudflare tunnel temp links

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
-  allowedDevOrigins: [
-    "microwave-lawrence-stuart-result.trycloudflare.com",
-    "files-airplane-prayers-burst.trycloudflare.com",
-    "modifications-lanka-span-cinema.trycloudflare.com",
-  ],
 };
 
 export default nextConfig;
